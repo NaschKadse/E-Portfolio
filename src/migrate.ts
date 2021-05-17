c4b6b2e268e458cac5fcc61f26a5209863bb87b2
Dominik Klein <klein-dominik@web.de>
2021-05-17 08:11:48 +0200

    STEP 05: Adding the orders model

--- src/migrate.ts
+++ src/migrate.ts
@@ -6,7 +6,7 @@ export async function migrate(args: string[]) {
 
   const app = new EPortfolioApplication();
   await app.boot();
-  await app.migrateSchema({existingSchema,  models:['Product']});
+  await app.migrateSchema({existingSchema,  models:['Product', 'Order']});
 
   // Connectors usually keep a pool of opened connections,
   // this keeps the process running even after all work is done.
---
+++ src/models/order.model.ts
@@ -0,0 +1,34 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Order extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  pieces: number;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  date: string;
+
+
+  constructor(data?: Partial<Order>) {
+    super(data);
+  }
+}
+
+export interface OrderRelations {
+  // describe navigational properties here
+}
+
+export type OrderWithRelations = Order & OrderRelations;
